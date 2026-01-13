c16604223484ee7c65c6f9de772e186266db6e1e
unknown <luivegmen@gmail.com>
2026-01-13 13:26:42 -0500

    Fix Pillow build error on Render

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@
 set -o errexit
 
 pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+
+# FORZAR BINARIOS (esto arregla Pillow)
+pip install --only-binary=:all: Pillow==9.5.0
+pip install -r requirements.txt --prefer-binary
 
 python manage.py collectstatic --noinput
 python manage.py migrate
