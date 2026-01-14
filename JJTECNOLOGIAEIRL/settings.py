4b687661841c71bd39d3fc94a1950344f14f3c11
unknown <luivegmen@gmail.com>
2026-01-14 17:22:13 -0500

    Fix ALLOWED_HOSTS for Render

--- JJTECNOLOGIAEIRL/settings.py
+++ JJTECNOLOGIAEIRL/settings.py
@@ -42,13 +42,13 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
+ALLOWED_HOSTS = [
+    "muchachitotec.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
-
-
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    ".onrender.com,localhost,127.0.0.1"
-).split(",")
 
 
 
