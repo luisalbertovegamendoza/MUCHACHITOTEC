f1387a3da70a99f9910b707f6690858187e9ac5e
unknown <luivegmen@gmail.com>
2026-01-14 00:15:48 -0500

    Fix build.sh remove forced Pillow

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ set -o errexit
 pip install --upgrade pip setuptools wheel
 
 # FORZAR BINARIOS (esto arregla Pillow)
-pip install --only-binary=:all: Pillow==9.5.0
 pip install -r requirements.txt --prefer-binary
 
 python manage.py collectstatic --noinput
